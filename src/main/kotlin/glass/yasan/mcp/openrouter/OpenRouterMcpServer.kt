9ada1b9f915915d03efcb7fa5fd1d99731f8670c
Yasan Glass <hello@yasan.glass>
2025-07-12 22:54:10 +0200

    Add `user-chat-completion` tool

--- src/main/kotlin/glass/yasan/mcp/openrouter/OpenRouterMcpServer.kt
+++ src/main/kotlin/glass/yasan/mcp/openrouter/OpenRouterMcpServer.kt
@@ -2,8 +2,11 @@ package glass.yasan.mcp.openrouter
 
 import glass.yasan.mcp.openrouter.client.OpenRouterClient
 import glass.yasan.mcp.openrouter.client.OpenRouterClientImpl
+import io.modelcontextprotocol.kotlin.sdk.CallToolResult
 import io.modelcontextprotocol.kotlin.sdk.Implementation
 import io.modelcontextprotocol.kotlin.sdk.ServerCapabilities
+import io.modelcontextprotocol.kotlin.sdk.TextContent
+import io.modelcontextprotocol.kotlin.sdk.Tool
 import io.modelcontextprotocol.kotlin.sdk.server.Server
 import io.modelcontextprotocol.kotlin.sdk.server.ServerOptions
 import io.modelcontextprotocol.kotlin.sdk.server.StdioServerTransport
@@ -12,8 +15,14 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.io.asSink
 import kotlinx.io.asSource
 import kotlinx.io.buffered
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.put
 
 const val ENV_OPENROUTER_API_KEY = "OPENROUTER_API_KEY"
+
+const val TOOL_INPUT_MODEL = "model"
+const val TOOL_INPUT_USER_MESSAGE = "user_message"
 
 fun main() {
     val openRouterApiKey = System.getenv(ENV_OPENROUTER_API_KEY)
@@ -50,8 +59,40 @@ fun createServer(openRouterApiKey: String): Server {
     val server = Server(
         serverInfo = info,
         options = options
-    )
-    // TODO add tools
+    ).apply {
+        addUserPromptTool(openRouterClient)
+    }
 
     return server
+}
+
+fun Server.addUserPromptTool(
+    openRouterClient: OpenRouterClient,
+) {
+    val inputScheme = Tool.Input(
+        buildJsonObject {
+            put(TOOL_INPUT_MODEL, "string")
+            put(TOOL_INPUT_USER_MESSAGE, "string")
+        }
+    )
+
+    addTool(
+        name = "user-chat-completion",
+        description = "Returns a chat completion response from OpenRouter for the user message OR an error on failure",
+        inputSchema = inputScheme,
+    ) { input ->
+        val model = input.arguments[TOOL_INPUT_MODEL]!!.jsonPrimitive.content
+        val userMessage = input.arguments[TOOL_INPUT_USER_MESSAGE]!!.jsonPrimitive.content
+
+        CallToolResult(
+            listOf(
+                TextContent(
+                    text = openRouterClient.userChatCompletion(
+                        model = model,
+                        userMessage = userMessage,
+                    ),
+                ),
+            ),
+        )
+    }
 }
